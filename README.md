e2960936d6b69a6517485b07cd2f43f4967816d5
Thiru Thirunavukarasu <thiru0130@gmail.com>
2015-07-10 22:42:32 -0400

    Command-line arg for number of lines working

--- README.md
+++ README.md
@@ -7,10 +7,12 @@ Shows the last *n* lines of the last modified file in a directory.
 ## Examples
 
 * The simplest way to run the app is to run it without any arguments
-* This will show the last 10 lines of the last modified file in the current directory
+* The following will show the last 10 lines of the last modified file in the
+  current directory
 
     tailest
 
-* This will show the last 23 lines of the last modified file in the current directory
+* This will show the last 23 lines of the last modified file in the current
+  directory
 
     tailest -n 23
--- lib/main.lisp
+++ lib/main.lisp
@@ -1,30 +1,70 @@
 (in-package :tailest)
 
-(defvar app-version 0.1)
-(defvar app-updated "Jul 4 2015")
+(defvar app-version 0.2)
+(defvar app-updated "Jul 10 2015")
+
+(defparameter debug-mode nil)
+(defparameter debug-args '("-a" "--help" "-n" "23"))
+
+(defparameter num-lines-default 30)
 
 (defun run ()
-  (let* ((target-dir (getcwd))
+  (if debug-mode (format t "Command-line args: ~A~%" (command-line-arguments)))
+  (let* ((cmd-args (if debug-mode debug-args (command-line-arguments)))
+         (parsed-args (parse-cmd-args cmd-args num-lines-default))
+         (target-dir (getcwd))
          (files (directory-files target-dir)))
+    (if debug-mode (format t "Parsed args: ~A~%" parsed-args))
     (if (null files)
       (format t "No files found in ~a~%" target-dir)
-      (format t "~{~a~^~%~}~%" (get-last-n-lines (get-latest-file files) 20)))))
+      (let ((num-lines (getf parsed-args :num-lines)))
+        (format t "~{~a~^~%~}~%"
+                (get-last-n-lines
+                  (get-latest-file files)
+                  (if (> num-lines 0)
+                    num-lines
+                    num-lines-default)))))))
+
+(defmacro pl=> (l k v)
+  `(setf (getf ,l ,k) ,v))
+
+(defun parse-cmd-args (cmd-args default-num-lines)
+  (let ((parsed-args `(:show-help nil
+                       :num-lines ,default-num-lines)))
+    (dolist (arg cmd-args)
+      (cond ((arg=? arg "--help" "-h")
+             (pl=> parsed-args :show-help t))
+
+             ((arg=? arg "--num-files" "-n")
+              (pl=> parsed-args :num-lines (get-num-lines cmd-args arg)))))
+    parsed-args))
+
+(defun arg=? (arg-val &rest arg-names)
+  (if (null arg-names) (error "No argument names provided."))
+  (find arg-val arg-names :test #'equalp))
+
+(defun get-num-lines (cmd-args arg-name)
+  (let ((arg-idx (position arg-name cmd-args :test #'equalp)))
+    (if (or (null arg-idx)
+            (< arg-idx 0)
+            (>= (+ 1 arg-idx) (length cmd-args)))
+      (values 0 0)
+      (parse-integer (nth (+ 1 arg-idx) cmd-args) :junk-allowed t))))
 
 (defun get-latest-file (files)
   (let ((sorted-files (stable-sort files #'> :key #'safe-file-write-date)))
     (car sorted-files)))
 
 (defun get-last-n-lines (file n)
+  (if debug-mode (format t "Num lines: ~A~%" n))
   (let ((last-lines (make-array n :initial-element nil))
         (idx 0)
         (final-lines nil))
     (with-open-file (stream file)
       (loop for line = (read-line stream nil)
             while line do
-              ;(format t "~a <= ~a~%" idx line)
               (setf (aref last-lines idx) line)
               (setf idx (roll-forward idx n))))
-    ;(format t "idx = ~a~%" idx)
     (setf idx (roll-backward idx n))
     (dotimes (i n)
       (unless (null (aref last-lines idx))
